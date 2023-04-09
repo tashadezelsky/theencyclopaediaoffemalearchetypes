8f592dbf676aa4e0019d0844f36af7cc73553bc8
tashadezelsky <77847692+tashadezelsky@users.noreply.github.com>
2023-04-09 00:31:11 -0400

    lotsa edits

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
+console.log('yo yo');
+
 console.log("please work");
+
+
+if (
+    localStorage.getItem("color-mode") === "blue" ||
+    (window.matchMedia("(prefers-color-scheme: blue)").matches &&
+      !localStorage.getItem("color-mode"))
+  ) {
+    document.documentElement.setAttribute("color-mode", "blue");
+  }
+
+/*-- this one changes picture colors
+
+function one() {
+    var x = document.getElementById("contentpink");
+    if (x.style.display === "none") {
+      x.style.display = "grid";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  /*-- this one changes text colors --*/
+
+  
+
+
+   /* $(document).ready(function(){
+
+        $("button").click(function(){
+          $("#contentpink").toggle();
+          $('p').css('color', '#FF00FF');
+        });
+        
+    }); */
+
+    /* $(document).ready(function () {
+        $("p.home").css("background-color", "blue");
+      }); */
