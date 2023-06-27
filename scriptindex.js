bef417e1651056559aea4d91e25587a4244c8a1f
tashadezelsky <77847692+tashadezelsky@users.noreply.github.com>
2023-06-26 23:26:04 -0400

    hide footer

--- scriptindex.js
+++ scriptindex.js
@@ -54,36 +54,6 @@ pinkModeToggle.addEventListener('click', () => {
   }
 });
 
-function setupTabs () {
-    document.querySelectorAll(".tabs_button").forEach(button => {
-        button.addEventListener("click", () => {
-            const sideBar = button.parentElement;
-            const tabsContainer = sideBar.parentElement;
-            const tabNumber = button.dataset.forTab;
-            const tabToActivate = tabsContainer.querySelector(`.tabs_content[data-tab="${tabNumber}"]`);
-
-            sideBar.querySelectorAll(".tabs_button").forEach(button => {
-                button.classList.remove("tabs_button--active");
-            });
-
-            tabsContainer.querySelectorAll(".tabs_content").forEach(tab => {
-                tab.classList.remove("tabs_content--active");
-            });
-
-            button.classList.add("tabs_button--active");
-                tabToActivate.classList.add("tabs_content--active");
-
-        });
-    });
-}
-
-document.addEventListener("DOMContentLoaded" , () => {
-    setupTabs(); 
-});
-
-
-
-
 /*-- this one changes picture colors
 
 function one() {
